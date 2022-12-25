102309cf6563f912278f6bc85e3315abbf426293
Diwas10 <rimaldiwas10@gmail.com>
2022-12-25 21:43:21 +0545

    Add day 6

---
+++ 06a.py
@@ -0,0 +1,21 @@
+# Get the datastream from file
+with open("input/06.txt") as file:
+    data = file.read().rstrip()
+
+def find_marker_pos(data):
+
+    marker_buffer = []
+    for i in range(len(data)):
+        char = data[i]
+        # print(marker_buffer)
+        if len(marker_buffer) == 4:
+            return i
+        if char in marker_buffer:
+            char_idx = marker_buffer.index(char)
+            marker_buffer = marker_buffer[char_idx + 1:]
+        marker_buffer.append(char)
+
+
+print(find_marker_pos(data))
+            
+
---
+++ 06b.py
@@ -0,0 +1,21 @@
+# Get the datastream from file
+with open("input/06.txt") as file:
+    data = file.read().rstrip()
+
+def find_message_pos(data):
+
+    marker_buffer = []
+    for i in range(len(data)):
+        char = data[i]
+        print(marker_buffer)
+        if len(marker_buffer) == 14:
+            return i
+        if char in marker_buffer:
+            char_idx = marker_buffer.index(char)
+            marker_buffer = marker_buffer[char_idx + 1:]
+        marker_buffer.append(char)
+
+
+print(find_message_pos(data))
+            
+
